cd8b06e2a4d41990e4305d9adee57c5c62de890d
jason88012 <jason40175032h@gmail.com>
2018-07-07 21:18:53 +0800

    Add SERVER_PREFIX note

--- util/conf.py
+++ util/conf.py
@@ -1,6 +1,6 @@
 import os
 
-SERVER_URL_PREFIX = os.getenv("SERVER_URL_PREFIX", "")
+SERVER_URL_PREFIX = os.getenv("SERVER_URL_PREFIX", "[Put your IP/domain name Here]")
 
 MAX_URL_LEN = int(os.getenv("MAX_URL_LEN", "2000"))
 DB_EXPIRE_TIME = int(os.getenv("DB_EXPIRE_TIME", "31536000"))
